3eb53de68e84ed0bf72d48c19cc88eb19bea1859
K Tact <ktact.dev@gmail.com>
2023-04-19 20:27:13 +0900

    Add puts function for console string output

--- boot.s
+++ boot.s
@@ -28,12 +28,15 @@ ipl:
     ; Store the boot drive
     mov [BOOT.DRIVE], dl
 
-    push word 'A'
-    call putc
+    push .s0
+    call puts
     add sp, 2        ; Clean up the stack
 
     ; Infinite loop
     jmp $
+
+    ; Data definition
+.s0 db "Booting...", 0x0A, 0x0d, 0
 
 ; Function to output a character to the console
 putc:
@@ -54,6 +57,43 @@ putc:
     int 0x10         ; Video BIOS Call
 
     ; Restore the registers
+    pop bx
+    pop ax
+
+    ; Destruct the stack frame
+    mov sp, bp
+    pop bp
+
+    ret
+
+; Function to output string to the console
+puts:
+    ; Construct the stack frame
+               ; + 4 | a string to output (the argument of this function)
+               ; + 2 | a return address
+    push bp    ; + 0 | BP (original value)
+    mov bp, sp ; bp points + 0
+
+    ; Save the registers
+    push ax
+    push bx
+    push si
+
+    ; Process
+    mov si, [bp + 4] ; Get a string to output
+    mov ah, 0x0E
+    mov bx, 0x0000
+    cld
+.10L:         ; DO {
+    lodsb     ;   AL = (*SI)++
+    cmp al, 0 ;   IF AL == 0 THEN
+    je .10E   ;     BREAK
+    int 0x10  ;   Video BIOS Call
+    jmp .10L  ; } WHILE (1)
+.10E:
+
+    ; Restore the registers
+    pop si
     pop bx
     pop ax
 
