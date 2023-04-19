d7c19b0fadab90cd49975348862f5872fd23b31a
K Tact <ktact.dev@gmail.com>
2023-04-19 20:15:19 +0900

    Add putc function for console char output and replace 'A' output logic with putc call

--- boot.s
+++ boot.s
@@ -28,13 +28,40 @@ ipl:
     ; Store the boot drive
     mov [BOOT.DRIVE], dl
 
-    mov al, 'A'    ; AL = character to output
-    mov ah, 0x0E
-    mov bx, 0x0000 ; Set the page number/color of character to 0
-    int 0x10       ; Video BIOS Call
+    push word 'A'
+    call putc
+    add sp, 2        ; Clean up the stack
 
     ; Infinite loop
     jmp $
+
+; Function to output a character to the console
+putc:
+    ; Construct the stack frame
+               ; + 4 | a character to output (the argument of this function)
+               ; + 2 | a return address
+    push bp    ; + 0 | BP (original value)
+    mov bp, sp ; bp points + 0
+
+    ; Save the registers
+    push ax
+    push bx
+
+    ; Process
+    mov al, [bp + 4] ; Get a character to output
+    mov ah, 0x0E
+    mov bx, 0x0000   ; Set the page number/color of character to 0
+    int 0x10         ; Video BIOS Call
+
+    ; Restore the registers
+    pop bx
+    pop ax
+
+    ; Destruct the stack frame
+    mov sp, bp
+    pop bp
+
+    ret
 
 ALIGN 2, db 0
 BOOT:
